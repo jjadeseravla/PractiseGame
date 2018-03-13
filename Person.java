b564e656d6aa4aaddfce2546513e8aaf8a9dbe3a
jjade seravla <jadealvares@hotmail.co.uk>
2018-03-13 17:46:22 +0000

    new person class

---
+++ Person.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Person
+{
+	private String name;
+	public static void main(String[] args)
+	{
+		//Person Class
+	}
+	public static int numToes()
+	{
+		return 10;
+	}
+	public static String name()
+	{
+		return name;
+	}
+}
